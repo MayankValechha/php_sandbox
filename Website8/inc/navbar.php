0262267b9f98a8880f2e5df380d50d32ccb88a44
valechhamayank12@gmail.com <mayank.valechha@yahoo.com>
2019-01-31 15:58:20 +0530

    Navbar include file

--- Website8/inc/navbar.php
+++ Website8/inc/navbar.php
@@ -1,7 +1,11 @@
 <nav class="navbar navbar-default">
-    <div class="container">
-        <div class="navbar-header">
-            <a href="/php_sandbox/website8" class="navbar-brand">PHP Blog</a>
-        </div>
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="<?php echo ROOT_URL; ?>">PHP Blog</a>
     </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="<?php echo ROOT_URL; ?>">Home</a></li>
+      <li><a href="#">Add Post</a></li>
+    </ul>
+  </div>
 </nav>
