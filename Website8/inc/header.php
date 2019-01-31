98ad41a3b98aed155ec282090deddc9984d796bc
valechhamayank12@gmail.com <mayank.valechha@yahoo.com>
2019-01-31 15:56:23 +0530

    Container headear, footer and navbar includes

---
+++ Website8/inc/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Blog</title>
+    <link rel="stylesheet" href="https://bootswatch.com/3/sandstone/bootstrap.min.css">
+</head>
+<body>
+    <?php include 'navbar.php'; ?>
---
+++ Website8/inc/navbar.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-default">
+    <div class="container">
+        <div class="navbar-header">
+            <a href="/php_sandbox/website8" class="navbar-brand">PHP Blog</a>
+        </div>
+    </div>
+</nav>
