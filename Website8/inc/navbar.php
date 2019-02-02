1395bc4a6b626c1fa2ac996cdc57ae0e69c8f15b
valechhamayank12@gmail.com <mayank.valechha@yahoo.com>
2019-02-03 00:26:15 +0530

    Conatins header, footer and navbar includes

--- Website8/inc/navbar.php
+++ Website8/inc/navbar.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="<?php echo ROOT_URL; ?>">PHP Blog</a>
+      <a class="navbar-brand" href="<?php echo ROOT_URL;?>">PHP Blog</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="<?php echo ROOT_URL; ?>">Home</a></li>
-      <li><a href="#">Add Post</a></li>
+      <li><a href="http://localhost/php_sandbox/website8/">Home</a></li>
+      <li><a href="http://localhost/php_sandbox/website8/addpost.php">Add Post</a></li>
     </ul>
   </div>
 </nav>
